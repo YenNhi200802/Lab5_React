dfcdaed531da01f73b36d56c7e1786be190a114a
YenNhi200902 <nhiyen200802@gmail.com>
2024-09-30 03:24:20 +0700

    first commit

---
+++ screens/ConfirmOrder.js
@@ -0,0 +1,92 @@
+import React, {useState} from 'react';
+import {View, Text, Button, StyleSheet, Image} from 'react-native';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import {payConfirm} from '../store';
+import {useMyContextController} from '../store'; // Import Context
+
+const ConfirmOrder = () => {
+  const navigation = useNavigation();
+  const route = useRoute();
+  const {serviceName, servicePrice, imageUrl} = route.params;
+
+  const [controller] = useMyContextController();
+  const {userLogin} = controller;
+
+  const [confirming, setConfirming] = useState(false);
+
+  const userName = userLogin ? userLogin.name : 'Tên người dùng mẫu';
+
+  const handleConfirm = async () => {
+    try {
+      setConfirming(true);
+
+      const paymentSuccess = await payConfirm(
+        userName,
+        serviceName,
+        servicePrice,
+      );
+
+      if (paymentSuccess) {
+        console.log('Thanh toán thành công!');
+        navigation.navigate('Home');
+      } else {
+        console.log('Thanh toán thất bại.');
+      }
+    } catch (error) {
+      console.error('Lỗi khi xác nhận thanh toán:', error);
+    } finally {
+      setConfirming(false);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      {imageUrl && <Image source={{uri: imageUrl}} style={styles.image} />}
+      <View style={styles.textRow}>
+        <Text style={styles.label}>Dịch vụ:</Text>
+        <Text style={styles.text}>{serviceName}</Text>
+      </View>
+      <View style={styles.textRow}>
+        <Text style={styles.label}>Giá dịch vụ:</Text>
+        <Text style={styles.text}>{servicePrice} VNĐ</Text>
+      </View>
+      <Button
+        title={confirming ? 'Đang xác nhận...' : 'Xác nhận'}
+        onPress={handleConfirm}
+        disabled={confirming}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'center',
+  },
+  image: {
+    width: '100%',
+    height: 300,
+    marginBottom: 20,
+  },
+  label: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginLeft: 10,
+  },
+  text: {
+    fontSize: 20,
+    marginLeft: 10,
+  },
+  textRow: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  button: {
+    width: 400,
+    height: 50,
+  },
+});
+
+export default ConfirmOrder;
---
+++ screens/Register.js
@@ -0,0 +1,155 @@
+import {Alert, View, StyleSheet} from 'react-native';
+import {Button, HelperText, Text, TextInput} from 'react-native-paper';
+import {useState} from 'react';
+import firestore from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
+import COLORS from '../constants';
+
+const Register = ({navigation}) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPasswordConfirm, setShowPasswordConfirm] = useState(false);
+  const [address, setAddress] = useState('');
+  const [phone, setPhone] = useState('');
+
+  const hasErrorName = () => name === '';
+  const hasErrorEmail = () => !email.includes('@');
+  const hasErrorPassword = () => password.length < 6;
+  const hasErrorPasswordConfirm = () => passwordConfirm != password;
+  const hasErrorAddress = () => address === '';
+  const hasErrorrPhone = () => phone.length < 10;
+
+  const USERS = firestore().collection('USERS');
+
+  const handleCreateAccount = () => {
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(response => {
+        USERS.doc(email).set({
+          name,
+          email,
+          password,
+          address,
+          phone,
+          role: 'customer',
+        });
+        navigation.navigate('Login');
+      })
+      .catch(e => Alert.alert('Tài khoản tồn tại'));
+  };
+
+  return (
+    <View style={{flex: 1, padding: 20, width: 400}}>
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: 'bold',
+          alignSelf: 'center',
+          color: COLORS.pink,
+          marginTop: 20,
+          marginBottom: 20,
+        }}>
+        {' '}
+        Create New Account{' '}
+      </Text>
+      <TextInput
+        label="Full Name"
+        value={name}
+        onChangeText={setName}
+        style={styles.InputText}
+      />
+      <HelperText type="error" visible={hasErrorName()}>
+        Full name không được phép để trống
+      </HelperText>
+      <TextInput
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+        style={styles.InputText}
+      />
+      <HelperText type="error" visible={hasErrorEmail()}>
+        Địa chỉ email không hợp lệ
+      </HelperText>
+      <TextInput
+        style={styles.InputText}
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry={!showPassword}
+        right={
+          <TextInput.Icon
+            icon="eye"
+            onPress={() => setShowPassword(!showPassword)}
+          />
+        }
+      />
+      <HelperText type="error" visible={hasErrorPassword()}>
+        Password ít nhất 6 kí tự
+      </HelperText>
+      <TextInput
+        style={styles.InputText}
+        label={'Confirm Password'}
+        value={passwordConfirm}
+        onChangeText={setPasswordConfirm}
+        secureTextEntry={!showPasswordConfirm}
+        right={
+          <TextInput.Icon
+            icon="eye"
+            onPress={() => setShowPasswordConfirm(!showPasswordConfirm)}
+          />
+        }
+      />
+
+      <HelperText type="error" visible={hasErrorPasswordConfirm()}>
+        PasswordConfirm phải so khớp với password
+      </HelperText>
+      <TextInput
+        label={'Address'}
+        value={address}
+        onChangeText={setAddress}
+        style={styles.InputText}
+      />
+      <HelperText type="error" visible={hasErrorAddress()}>
+        Address không được phép để trống
+      </HelperText>
+      <TextInput
+        label={'Phone'}
+        value={phone}
+        onChangeText={setPhone}
+        style={styles.InputText}
+      />
+      <HelperText type="error" visible={hasErrorrPhone()}>
+        Phone phải đủ 10 số
+      </HelperText>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Button
+          style={{
+            backgroundColor: COLORS.pink,
+          }}
+          mode="contained"
+          onPress={handleCreateAccount}>
+          Create New Account
+        </Button>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  InputText: {
+    borderWidth: 1,
+    borderColor: 'pink',
+    backgroundColor: 'white',
+    borderRadius: 15,
+  },
+});
+
+export default Register;
